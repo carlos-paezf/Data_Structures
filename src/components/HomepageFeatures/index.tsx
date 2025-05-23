41b01888950e6f0dc85a55ae2cb12c53e70c27f4
Carlos David Páez Ferreira <cpaezferreira@gmail.com>
2025-05-23 08:55:21 -0500

    docs: correct item content

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,33 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
 	{
-		title: 'Easy to Use',
+		title: 'Documentación accesible',
 		Svg: require( '@site/static/img/undraw_docusaurus_mountain.svg' ).default,
 		description: (
 			<>
-				Docusaurus was designed from the ground up to be easily installed and
-				used to get your website up and running quickly.
+				Documentación abierta para estudiantes, profesores y entusiastas de la
+				informática. Aprende sobre estructuras de datos y algoritmos en un
+				entorno colaborativo y accesible.
 			</>
 		),
 	},
 	{
-		title: 'Focus on What Matters',
+		title: 'Aprende a tu ritmo',
 		Svg: require( '@site/static/img/undraw_docusaurus_tree.svg' ).default,
 		description: (
 			<>
-				Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-				ahead and move your docs into the <code>docs</code> directory.
+				Aprende a tu propio ritmo con ejemplos prácticos. La
+				documentación está diseñada para ser fácil de seguir y entender.
 			</>
 		),
 	},
 	{
-		title: 'Powered by React',
+		title: 'Repositorio abierto',
 		Svg: require( '@site/static/img/undraw_docusaurus_react.svg' ).default,
 		description: (
 			<>
-				Extend or customize your website layout by reusing React. Docusaurus can
-				be extended while reusing the same header and footer.
+				Contribuye al proyecto y mejora la documentación. Tu participación es
+				valiosa y ayuda a construir una comunidad más fuerte.
 			</>
 		),
 	},
